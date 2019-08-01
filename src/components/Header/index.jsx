0036c9384928d69402cd1e4d1863399d3040d0f0
Johee Chung <johee.chung@nike.com>
2019-08-01 08:27:41 -0700

    Add location

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -6,12 +6,15 @@ import './index.css';
 function Header() {
   return (
     <div className='header-container red-background'>
-      <Logo />
+      <div>
+        <Logo />
+      </div>
       <div className='header-title'>
         <SocialMedia />
         <div className='white header-title-text'>
           ROLLER DERBY
         </div>
+        <div className='header-location green'>in Oregon (Hood River/Cascade Locks)</div>
       </div>
     </div>
   );
