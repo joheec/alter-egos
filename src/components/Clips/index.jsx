1b5fc007bc49ab30acaff81b76d4ccfc67c2e4fd
Johee Chung <johee.chung@nike.com>
2019-07-31 22:14:58 -0700

    Clips content

--- src/components/Clips/index.jsx
+++ src/components/Clips/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import IssIcon from '../../assets/iss-corner.png';
+import RegularmanIcon from '../../assets/regularman.png';
 import './index.css';
 
 function Clips() {
   return (
     <div className='clips-container'>
-      <h2>Check Out Moments from Our Bouts!</h2>
+      <h1>Check Out Moments from Our Bouts!</h1>
       <div className='photo-credit'>
         Photography Credit:
         <a
@@ -14,31 +16,47 @@ function Clips() {
           className='purple'
         > Regularman Photography</a>
       </div>
-      <div className='clip-link-container'>
-        <a
-          href='https://www.regularmanphotography.com/RollerDerby/2019/20119-06-29-Storm-City-v-Alter-Egos'
-          target='_blank'
-          rel='noreferrer noopener'
-        >
-          June 29th, 2019: Alter Egos vs Storm City
-        </a>
+      <div className='clips-event-container'>
+        <h2>June 29th, 2019: Alter Egos vs Storm City</h2>
+        <div className='clips-links-container'>
+          <a
+            href='https://www.regularmanphotography.com/RollerDerby/2019/20119-06-29-Storm-City-v-Alter-Egos'
+            target='_blank'
+            rel='noreferrer noopener'
+          >
+            <img alt='Regularman Photography' src={RegularmanIcon} />
+            Photos
+          </a>
+          <a
+            href='https://www.youtube.com/playlist?list=PLoMcY-NcwMAuSF2h-kVVRe97hsWnYQgY4'
+            target='_blank'
+            rel='noreferrer noopener'
+          >
+            <img alt={'Is\'s Corner'} src={IssIcon} />
+            Is's Announcer Corner!
+          </a>
+        </div>
       </div>
-      <div className='clip-link-container'>
+      <div className='clips-event-container'>
+        <h2>April 12th, 2019: Alter Egos vs Gorge</h2>
         <a
           href='https://www.regularmanphotography.com/RollerDerby/2019/2019-04-12-Gorge-v-Alter-Egos'
           target='_blank'
           rel='noreferrer noopener'
         >
-          April 12th, 2019: Alter Egos vs Gorge
+          <img alt='Regularman Photography' src={RegularmanIcon} />
+          Photos
         </a>
       </div>
-      <div className='clip-link-container'>
+      <div className='clips-event-container'>
+        <h2>March 30th, 2019: Alter Egos vs Rainy City</h2>
         <a
           href='https://www.regularmanphotography.com/RollerDerby/2019/2019-03-30-Rainy-City-v-Alter-Egos'
           target='_blank'
           rel='noreferrer noopener'
         >
-          March 30th, 2019: Alter Egos vs Rainy City
+          <img alt='Regularman Photography' src={RegularmanIcon} />
+          Photos
         </a>
       </div>
     </div>
