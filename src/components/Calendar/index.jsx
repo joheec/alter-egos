37a0bcbcc43e91af6a0b17dd0220c256ee5675d6
Johee Chung <johee.chung@nike.com>
2019-08-02 22:44:10 -0700

    Calendar fix

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -5,8 +5,14 @@ function Calendar() {
   return (
     <div className='calendar-container'>
       <iframe
+        className='desktop'
         title='Alter Egos Roller Derby calendar'
-        src='https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23ffffff&amp;ctz=America%2FLos_Angeles&amp;src=am9oZWUuY2h1bmdAZ21haWwuY29t&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%239E69AF&amp;color=%23616161&amp;showPrint=0&amp;showDate=1&amp;showCalendars=0&amp;showTitle=0&amp;showNav=1&amp;mode=WEEK'
+        src='https://calendar.google.com/calendar/embed?src=alteregosrollerderby%40gmail.com&ctz=America%2FLos_Angeles;showPrint=0&amp;showDate=1&amp;showCalendars=0&amp;showTitle=0&amp;showNav=1&amp;mode=MONTH'
+      ></iframe>
+      <iframe
+        className='mobile'
+        title='Alter Egos Roller Derby calendar'
+        src='https://calendar.google.com/calendar/embed?src=alteregosrollerderby%40gmail.com&ctz=America%2FLos_Angeles;showPrint=0&amp;showDate=1&amp;showCalendars=0&amp;showTitle=0&amp;showNav=1&amp;mode=AGENDA'
       ></iframe>
     </div>
   );
