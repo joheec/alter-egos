a208ea243c69647239c4e3f9c09cec97bd5f129d
Johee Chung <johee.chung@nike.com>
2019-08-02 18:23:25 -0700

    Calendar

--- src/components/Merch/index.jsx
+++ src/components/Merch/index.jsx
@@ -6,19 +6,21 @@ function Merch() {
   return (
     <div className='merch-container'>
       <h2 className='merch-title'>Available for Purchase at Practices, Scrimmages, and Bouts</h2>
-      <div className='merch-item'>
-        <a
-          href='https://www.bonfire.com/alteregos/'
-          target='_blank'
-          rel='noreferrer noopener'
-        >
-          <div className='merch-item-image'>
-            <img src={Tshirt1} alt='t-shirt' />
-          </div>
-          <div className='merch-item-price'>
-            $21.99
-          </div>
-        </a>
+      <div className='merch-items'>
+        <div className='merch-item'>
+          <a
+            href='https://www.bonfire.com/alteregos/'
+            target='_blank'
+            rel='noreferrer noopener'
+          >
+            <div className='merch-item-image'>
+              <img src={Tshirt1} alt='t-shirt' />
+            </div>
+            <div className='merch-item-price'>
+              $21.99
+            </div>
+          </a>
+        </div>
       </div>
     </div>
   );
