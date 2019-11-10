475e2362c4e04d491ca98161129897d68e00e942
Johee Chung <johee.chung@nike.com>
2019-11-10 02:23:39 -0800

    Update homepage location and photo styles

--- src/components/Photos/index.jsx
+++ src/components/Photos/index.jsx
@@ -1,27 +1,53 @@
 import React from 'react';
 import Popup from '../Popup';
-import Photo1 from './2019-07-09-001.jpg';
+import Photo1 from './2019-06-30.jpg';
 import Photo2 from './2019-07-09-002.jpg';
-import Photo3 from './2019-06-30.jpg';
+import Photo3 from './2019-07-09-001.jpg';
+import Photo4 from './2019-10-06.jpg';
 import './styles.css';
+
+const Credit = ({ link, name }) => (
+  <div className='photos-credit'>
+    Photography Credit:
+    <a
+      href={link}
+      target='_blank'
+      rel='noreferrer noopener'
+    >{' ' + name}</a>
+  </div>
+);
 
 const Photos = () => {
   return (
     <div className='photos-container'>
-      <div className='photos-album'>
-        <img src={Photo1} alt='July 2019 Team' />
-        <img src={Photo2} alt='July 2019 Team' />
-        <img src={Photo3} alt='June 2019 Team' />
-      </div>
-      <div className='photos-credit'>
-        Photography Credit:
-        <a
-          href='https://www.regularmanphotography.com/'
-          target='_blank'
-          rel='noreferrer noopener'
-          className='purple'
-        > Regularman Photography</a>
-      </div>
+      <div>October 2019 Team</div>
+      <div>Alter Egos vs Cherry City</div>
+      <Credit
+        link="https://www.facebook.com/FactorKites/"
+        name="Factor Kites Photography"
+      />
+      <img src={Photo4} alt='October 2019 Team' />
+      <div>July 2019 Team</div>
+      <div>Alter Egos vs Walla Walla & Atomic City</div>
+      <Credit
+        link="https://www.regularmanphotography.com/"
+        name="Regularman Photography"
+      />
+      <img src={Photo3} alt='July 2019 Team' />
+      <div>June 2019 Team</div>
+      <div>Alter Egos vs Storm City</div>
+      <Credit
+        link="https://www.regularmanphotography.com/"
+        name="Regularman Photography"
+      />
+      <img src={Photo2} alt='July 2019 Team' />
+      <div>June 2019 Team</div>
+      <div>Alter Egos vs Storm City</div>
+      <Credit
+        link="https://www.regularmanphotography.com/"
+        name="Regularman Photography"
+      />
+      <img src={Photo1} alt='June 2019 Team' />
     </div>
   );
 };
