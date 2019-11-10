41bbe35314acc3c18350e172dd623ca6307d1793
Johee Chung <johee.chung@nike.com>
2019-11-10 01:35:44 -0800

    Update calendar styles

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -7,7 +7,7 @@ function Calendar() {
     <div className='calendar-container'>
         <iframe
             title='Alter Egos Roller Derby calendar'
-            src='https://calendar.google.com/calendar/embed?src=alteregosrollerderby%40gmail.com&ctz=America%2FLos_Angeles;showPrint=0&amp;showDate=1&amp;showCalendars=0&amp;showTitle=0&amp;showNav=1&amp;mode=AGENDA'
+            src='https://calendar.google.com/calendar/embed?src=alteregosrollerderby%40gmail.com&ctz=America%2FLos_Angeles;showPrint=0&amp;showDate=0&amp;showCalendars=0&amp;showTitle=0&amp;showNav=1&amp;mode=AGENDA'
         ></iframe>
     </div>
   );
