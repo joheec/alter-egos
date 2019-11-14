c72319dca1fde7cf4c96f11d3d2a3fbaab0744e2
Johee Chung <johee.chung@nike.com>
2019-11-14 12:37:43 -0800

    Add merch sales events

--- src/components/Merch/index.jsx
+++ src/components/Merch/index.jsx
@@ -6,7 +6,59 @@ import './styles.css';
 function Merch() {
   return (
     <div className='merch-container'>
-      <h2 className='merch-title'>Available for Purchase at Practices, Scrimmages, and Bouts</h2>
+      <h2 className='merch-title'>Available for Purchase at...</h2>
+      <div className="merch-sales">
+        <div className="merch-sales-row merch-sales-header">
+          <div className="merch-sales-date">date</div>
+          <div className="merch-sales-event">event</div>
+          <div className="merch-sales-location">location</div>
+        </div>
+        <div className="merch-sales-row">
+          <div className="merch-sales-date">Thurs Nov 14, 2019 @ 8:30pm</div>
+          <div>
+            <a
+              href="https://www.facebook.com/events/1458067034340378/"
+              target='_blank'
+              rel='noreferrer noopener'
+            >Alter Egos Scrimmage</a>
+          </div>
+          <div className="merch-sales-location">
+            <a
+              href="https://goo.gl/maps/AZbSCNFLxfUT8GHCA"
+              target='_blank'
+              rel='noreferrer noopener'
+            >R A Brown Middle School</a>
+          </div>
+        </div>
+        <div className="merch-sales-row">
+          <div className="merch-sales-date">Fri Nov 22, 2019 @ 7am-5pm</div>
+          <div className="merch-sales-event">
+            <a
+              href="https://www.facebook.com/events/466858470599664/"
+              target='_blank'
+              rel='noreferrer noopener'
+            >Holiday Craft Sale</a>
+          </div>
+          <div className="merch-sales-location">
+            <a
+              href="https://goo.gl/maps/pzebdGqiMsm9QZJa9"
+              target='_blank'
+              rel='noreferrer noopener'
+            >Legacy Salmon Creek Medical Center</a>
+          </div>
+        </div>
+        <div className="merch-sales-row">
+          <div className="merch-sales-date">Sat Dec 7, 2019 @ 9am-4pm</div>
+          <div className="merch-sales-event">Wahtonka Holiday Bazaar</div>
+          <div className="merch-sales-location">
+            <a
+              href="https://goo.gl/maps/d1nkge8w34tAKBta8"
+              target='_blank'
+              rel='noreferrer noopener'
+            >Wahtonka Community School</a>
+          </div>
+        </div>
+      </div>
       <div className='merch-items'>
       {
         inventory.map(({ alt, image, link, price }) => (
